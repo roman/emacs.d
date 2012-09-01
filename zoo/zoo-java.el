2f32309ea7e737575f4324b7867ae6a03ecb728e
Roman Gonzalez <romanandreg@gmail.com>
2012-09-01 01:05:03 +0000

    Making flymake work correctly with java-checkstyle

--- zoo/zoo-java.el
+++ zoo/zoo-java.el
@@ -1,4 +1,3 @@
-
 ;; Flymake setup for checkstyle.
 ;;
 ;; This setup of flymake assumes there is a checkstyle binary
@@ -8,7 +7,7 @@
 
 (require 'flymake)
 
-(defun flymake-java-checkstyle-init ()
+(defun zoo/flymake-java-checkstyle-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
                      'flymake-create-temp-with-folder-structure
                      ; ^ Don't create a local file with the _flymake postfix
@@ -16,17 +15,45 @@
          (local-file (file-relative-name
                       temp-file
                       (file-name-directory buffer-file-name))))
-    (list "checkstyle -c /usr/share/checkstyle/sun_checks.xml " (list local-file))))
+    (list "checkstyle" (list "-c"
+                             "/usr/share/checkstyle/sun_checks.xml"
+                             local-file))))
 
-(setq flymake-allowed-file-name-masks
-      (cons '(".+\\.java$"
-              flymake-java-checkstyle-init
+(defvar zoo/flymake-java-checkstyle-err-pattern-no-column
+  '("^\\(.+\\):\\([0-9]+\\): \\(.+\\)$" 1 2 nil 3))
+
+(defvar zoo/flymake-java-checkstyle-err-pattern-with-column
+  '("^\\(.+\\):\\([0-9]+\\):\\([0-9]+\\): \\(.+\\)$" 1 2 3 4))
+
+(defun zoo/flymake-java-checkstyle-load ()
+  (interactive)
+
+  ;; Setting the current buffer to execute the java-checkstyle prg
+  (set (make-local-variable 'flymake-allowed-file-name-masks)
+       '(("." zoo/flymake-java-checkstyle-init
               flymake-simple-cleanup
-              flymake-get-real-file-name)
-            flymake-allowed-file-name-masks))
+              flymake-get-real-file-name)))
 
-;; Checkstyle error message format is already supported in
-;; flymake-err-line-patterns so we don't need to add a new
-;; entry there
+  ;; Setting error patterns regexps
+  (set (make-local-variable 'flymake-err-line-patterns)
+       (list zoo/flymake-java-checkstyle-err-pattern-no-column
+             zoo/flymake-java-checkstyle-err-pattern-with-column))
+
+  ;; Only allowing flymake mode when the binary we need is there
+  (if (executable-find "checkstyle")
+      (progn
+        (flymake-mode t))
+      (message "Not enabling flymake: checkstyle command not found")))
+
+;; Add all the setup of flymake in a hook
+(defun zoo/java-mode-hook ()
+  (interactive)
+  (zoo/flymake-java-checkstyle-load)
+  (defadvice flymake-post-syntax-check
+    (before flymake-force-check-was-interrupted)
+    (setq flymake-check-was-interrupted t))
+  (ad-activate 'flymake-post-syntax-check))
+
+(add-hook 'java-mode-hook 'zoo/java-mode-hook)
 
 (provide 'zoo-java)
