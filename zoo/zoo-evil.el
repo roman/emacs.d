70110dfb5b115288c876bf67c5e99718ca3a17ce
Roman Gonzalez <romanandreg@gmail.com>
2012-09-17 20:45:02 -0700

    Adding better support for evil-pl-modeline

--- zoo/zoo-evil.el
+++ zoo/zoo-evil.el
@@ -1,3 +1,5 @@
+(require 'zoo-org)
+
 ;;;;;;;;;;;;;;;;;
 ;; jk binding
 ;;;;;;;;;;;;;;;;;
@@ -15,7 +17,9 @@
       (cond
        ((null evt)
           (message ""))
-       ((and (integerp evt) (char-equal evt ?k))
+       ((and (integerp evt)
+             (char-equal evt ?k))
+          ;; remove the j character
           (delete-char -1)
           (set-buffer-modified-p modified)
           (push 'escape unread-command-events))
@@ -23,194 +27,154 @@
           (setq unread-command-events (append unread-command-events
                                               (list evt))))))))
 
-; Adding the binding for the j character, then
-; the k is handled on the function
-(define-key
-  evil-insert-state-map
-  "j"
-  #'zoo/jk-to-normal-mode)
+;;;;;;;;;;;;;;
+;; Insert Mode
+;;;;;;;;;;;;;;
+;;
+;; Adding the binding for the j character, then
+;; the k is handled on the function
+(define-key evil-insert-state-map "j" #'zoo/jk-to-normal-mode)
 
-;;;;;;;;;;;;;;;;;
-;; ESC Warning
-;;;;;;;;;;;;;;;;;
-
-;(evil-define-command zoo/esc-warning (arg)
-;  "Wait for further keys within `evil-esc-delay'.
-;   Otherwise send [escape]."
-;  :repeat ignore
-;  (interactive "P")
-;  (if (sit-for evil-esc-delay t)
-;      (progn
-;        (push 'escape unread-command-events)
-;        (when defining-kbd-macro
-;          ;; we need to replace the ESC by 'escape in the currently
-;          ;; defined keyboard macro
-;          (evil-save-echo-area
-;            (end-kbd-macro)
-;            (setq last-kbd-macro (vconcat last-kbd-macro [escape]))
-;            (start-kbd-macro t t))))
-;    (push last-command-event unread-command-events)
-;    ;; preserve prefix argument
-;    (setq prefix-arg arg))
-;  ;; disable interception for the next key sequence
-;  (message "you should use 'jk' instead of ESC")
-;  (evil-esc-mode -1)
-;  (setq this-command last-command))
-;
-;(define-key
-;  evil-esc-map
-;  (kbd "ESC")
-;  #'zoo/esc-warning)
-
-;;;;;;;;;;;;;;;;;
-;; ESC extras
-;;;;;;;;;;;;;;;;;
-
-; Make <ESC> quit almost everything...
-; As seen on:
-; * http://stackoverflow.com/questions/8483182/emacs-evil-mode-best-practice
-;(define-key evil-normal-state-map [escape] 'keyboard-quit)
-;(define-key evil-visual-state-map [escape] 'keyboard-quit)
-;(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-;(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-;(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-;(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-;(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-
+;;;;;;;;;;;;;;
+;; Normal Mode
+;;;;;;;;;;;;;;
+;;
+;; Undo visualize tree on ",u" when on normal mode
+(define-key evil-normal-state-map (kbd ",u") #'undo-tree-visualize)
+;; Find defined symbols using ,. in normal mode instead of M-. in
+;; insert mode
+(define-key evil-normal-state-map (kbd ",.") 'find-tag)
 
 ;;;;;;;;;;;;;;;;;
 ;; Custom highlights for insert and normal mode
 ;; in the modeline
 ;;;;;;;;;;;;;;;;;
-
-(defun zoo/org-clocking-p ()
-  (interactive)
-  (and (fboundp 'org-clocking-p)
-       (org-clocking-p)))
-
+;;
 ;; We don't want evil's default for evil-mode-line-format,
 ;; so we are going to append it ourselves at the
 ;; start of the mode-line-format list
 (setq evil-mode-line-format nil)
-(setq mode-line-clock-in-tag t)
-(defadvice evil-refresh-mode-line (after zoo-evil-refresh-mode-line activate)
+(setq zoo-mode-line-clock-in-tag t)
+
+(defun zoo/evil-refresh-mode-line ()
+  (interactive)
   (add-to-list 'mode-line-format " ")
-  (when (and (boundp 'mode-line-clock-in-tag)
+  (when (and (boundp 'zoo-mode-line-clock-in-tag)
              (not (zoo/org-clocking-p)))
-    (add-to-list 'mode-line-format '(:eval mode-line-clock-in-tag)))
+    (add-to-list 'mode-line-format
+                 '(:eval zoo-mode-line-clock-in-tag)))
   (add-to-list 'mode-line-format
                '(:eval evil-mode-line-tag))
   (force-mode-line-update))
 
-(defun zoo/highlight-clock-in ()
+(defun zoo/highlight-org-clock ()
   (interactive)
-  (if (and (boundp 'mode-line-clock-in-tag)
-           mode-line-clock-in-tag
+  (if (and (boundp 'zoo-mode-line-clock-in-tag)
+           zoo-mode-line-clock-in-tag
            (not (zoo/org-clocking-p)))
-      (setq mode-line-clock-in-tag
+      (setq zoo-mode-line-clock-in-tag
             (propertize " CLOCK-IN "
                         'face
                         '(:background "#FF3366"
                           :foreground "white"
                           'bold)))
-    (when mode-line-clock-in-tag
-        (setq mode-line-clock-in-tag ""))))
+    (when zoo-mode-line-clock-in-tag
+        ;; ^ set it to empty if it is actually defined
+        (setq zoo-mode-line-clock-in-tag ""))))
 
-(defun zoo/highlight-insert-mode ()
-  (interactive)
-  (set-face-foreground 'modeline "white")
-  (set-face-background 'modeline "#0087AF")
-  (setq evil-insert-state-tag
-        (propertize " I "
-                    'face
-                    '(:background "white"
-                      :foreground "#005F87"
-                      'bold))))
+(defmacro zoo/make-evil-highlight-mode (evil-mode
+                                        modeline-foreground
+                                        modeline-background
+                                        evil-tag-foreground
+                                        evil-tag-background
+                                        evil-tag-text)
+  (let ((function-name  (intern (format "zoo/highlight-%s-mode" evil-mode)))
+        (tag-var-name   (intern (format "evil-%s-state-tag" evil-mode)))
+        (evil-hook-name (intern (format "evil-%s-state-entry-hook"
+                                        evil-mode))))
+    `(progn
+       ;; Create a function that will specify the
+       ;; highlight for a evil-mode
+       (defun ,function-name ()
+         (interactive)
+         (set-face-foreground 'modeline ,modeline-foreground)
+         (set-face-background 'modeline ,modeline-background)
+         (setq ,tag-var-name
+               (propertize ,evil-tag-text
+                           'face
+                           '(:background ,evil-tag-background
+                             :foreground ,evil-tag-foreground
+                           'bold))))
+       ;; Call the function for the simple initialization
+       (,function-name)
+       ;; Add a hook for the mode entry
+       (add-hook ',evil-hook-name
+                 ',function-name)
+       ;; Add a hook to highlight the clock
+       (add-hook ',evil-hook-name
+                 'zoo/highlight-org-clock))))
 
-(defun zoo/highlight-emacs-mode ()
-  (interactive)
-  ;; modeline
-  (set-face-foreground 'modeline "black")
-  (set-face-background 'modeline "#E6E5E4")
-  ;; emacs state tag
-  (setq evil-emacs-state-tag
-        (propertize " E "
-                    'face
-                    '(:background "#5F005F"
-                      :foreground "white"
-                      'bold))))
 
-(defun zoo/highlight-normal-mode ()
-  (interactive)
-  ;; modeline
-  (set-face-foreground 'modeline "black")
-  (set-face-background 'modeline "#E6E5E4")
-  ;; normal state tag
-  (setq evil-normal-state-tag
-        (propertize " N "
-                    'face
-                    '(:background "#AFD700"
-                      :foreground "#005F00"
-                      'bold))))
+(zoo/make-evil-highlight-mode "insert"    ; mode name
+                              "white"     ; modeline foreground
+                              "#0087AF"   ; modeline background
+                              "#005F87"   ; mode foreground
+                              "white"     ; mode background
+                              " I ")      ; mode tag
 
-(defun zoo/highlight-visual-mode ()
-  (interactive)
-  (set-face-foreground 'modeline "black")
-  (set-face-background 'modeline "#E6E5E4")
-  (setq evil-visual-state-tag
-        (propertize " VISUAL "
-                    'face
-                    '(:background "#FF8700"
-                      :foreground "#870000"
-                      'bold))))
+(zoo/make-evil-highlight-mode "normal"
+                              "black"
+                              "#E6E5E4"
+                              "#005F00"
+                              "#AFD700"
+                              " N ")
 
-(defun zoo/highlight-replace-mode ()
-  (interactive)
-  (set-face-foreground 'modeline "black")
-  (set-face-background 'modeline "#E6E5E4")
-  (setq evil-replace-state-tag
-        (propertize " REPLACE "
-                    'face
-                    '(:background "#D70000"
-                      :foreground "white"
-                      'bold))))
+(zoo/make-evil-highlight-mode "emacs"
+                              "black"
+                              "#E6E5E4"
+                              "white"
+                              "#5F005F"
+                              " E ")
 
-(defun zoo/highlight-motion-mode ()
-  (interactive)
-  (set-face-foreground 'modeline "black")
-  (set-face-background 'modeline "#E6E5E4")
-  (setq evil-motion-state-tag
-        (propertize " MOTION "
-                    'face
-                    '(:background "#14AACC"
-                      :foreground "white"
-                      'bold))))
+(zoo/make-evil-highlight-mode "visual"
+                              "black"
+                              "#E6E5E4"
+                              "#870000"
+                              "#FF8700"
+                              " VISUAL ")
 
-(add-hook 'evil-insert-state-entry-hook  'zoo/highlight-insert-mode)
-(add-hook 'evil-insert-state-entry-hook  'zoo/highlight-clock-in)
+(zoo/make-evil-highlight-mode "replace"
+                              "black"
+                              "#E6E5E4"
+                              "white"
+                              "#D70000"
+                              " REPLACE ")
 
-(add-hook 'evil-emacs-state-entry-hook   'zoo/highlight-emacs-mode)
-(add-hook 'evil-emacs-state-entry-hook   'zoo/highlight-clock-in)
+(zoo/make-evil-highlight-mode "motion"
+                              "black"
+                              "#E6E5E4"
+                              "white"
+                              "#14AACC"
+                              " MOTION ")
 
-(add-hook 'evil-normal-state-entry-hook  'zoo/highlight-normal-mode)
-(add-hook 'evil-normal-state-entry-hook  'zoo/highlight-clock-in)
+(add-hook 'org-clock-in-hook   'zoo/highlight-org-clock)
+(add-hook 'org-clock-in-hook   'zoo/evil-refresh-mode-line)
 
-(add-hook 'evil-visual-state-entry-hook  'zoo/highlight-visual-mode)
-(add-hook 'evil-visual-state-entry-hook  'zoo/highlight-clock-in)
-
-(add-hook 'evil-replace-state-entry-hook  'zoo/highlight-replace-mode)
-(add-hook 'evil-replace-state-entry-hook  'zoo/highlight-clock-in)
-
-(add-hook 'evil-motion-state-entry-hook  'zoo/highlight-motion-mode)
-(add-hook 'evil-motion-state-entry-hook  'zoo/highlight-clock-in)
+(add-hook 'org-clock-out-hook  'zoo/highlight-org-clock)
+(add-hook 'org-clock-out-hook  'zoo/evil-refresh-mode-line)
 
 (global-set-key (kbd "<f7> e") 'evil-emacs-state)
 (global-set-key (kbd "<f7> n") 'evil-normal-state)
 
-(zoo/highlight-insert-mode)
-(zoo/highlight-normal-mode)
-(zoo/highlight-emacs-mode)
+(defadvice evil-refresh-mode-line
+  (before zoo-evil-refresh-mode-line activate)
+  (zoo/evil-refresh-mode-line))
+
+(defadvice evil-mode-enable-in-buffers
+  (after zoo-evil-mode-enable-in-buffers activate)
+  (zoo/evil-refresh-mode-line))
+
 (evil-mode 1)
-(surround-mode 1)
+(global-surround-mode 1)
 
 (provide 'zoo-evil)
