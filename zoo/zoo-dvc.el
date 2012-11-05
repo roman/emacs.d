d4c76917bc65837bf9ef225f9bb483fe15ad015d
Roman Gonzalez <romanandreg@gmail.com>
2012-11-05 03:20:59 +0000

    Adding keybindings for magit status mode on evil

--- zoo/zoo-dvc.el
+++ zoo/zoo-dvc.el
@@ -1,11 +1,20 @@
 (require 'evil)
 
+(defun zoo/magit-mode ()
+  (interactive)
+  (evil-define-key 'motion magit-status-mode-map
+    (kbd "RET") 'magit-visit-item))
+
 (defun zoo/dvc-mode ()
   (interactive)
-  (evil-define-key 'normal diff-mode-map
+  (evil-define-key 'motion diff-mode-map
     (kbd "q") 'quit-window
     (kbd "i") 'diff-goto-source))
 
+(evil-set-initial-state 'diff-mode 'motion)
+(evil-set-initial-state 'magit-status-mode 'motion)
+
+(add-hook 'magit-status-mode-hook 'zoo/magit-mode)
 (add-hook 'dvc-mode-hook 'zoo/dvc-mode)
 
 (provide 'zoo-dvc)
