f725ebf4f625514b98ec036c2811519b5a31308f
Roman Gonzalez <romanandreg@gmail.com>
2012-11-04 19:11:40 -0800

    Improvments on haskell config

--- zoo/zoo-haskell.el
+++ zoo/zoo-haskell.el
@@ -32,8 +32,9 @@
 (defun zoo/flymake-haskell-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
                      'flymake-create-temp-with-folder-structure))
-         (local-file (file-relative-name temp-file
-                                         (file-name-directory buffer-file-name))))
+         (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
     (list "ghc-mod" (list zoo/ghc-mod-subcmd local-file))))
 
 (defun zoo/flymake-haskell-load ()
@@ -61,9 +62,12 @@
 ;; Using `zoo.path/rfind-file` instead of `locate-dominating-file`
 ;; because the latter doesn't accept a regexp as the file name
 (defun zoo/is-cabal-dev-present? ()
-  (let ((current-dir (zoo.path/pwd)))
-    (and (zoo.path/rfind-file "*.cabal" current-dir)
-         (zoo.path/rfind-file "cabal-dev" current-dir))))
+  (let* ((current-dir (zoo.path/pwd))
+         (cabal-dev-folder (zoo.path/rfind-dir "cabal-dev" current-dir))
+         (cabal-file-folder (zoo.path/rfind-dir "*.cabal" current-dir)))
+    (and (not (null cabal-dev-folder))
+         (not (null cabal-file-folder))
+         (string= cabal-dev-folder cabal-file-folder))))
 
 (defun zoo/haskell-set-compile-command ()
   (if (zoo/is-cabal-dev-present?)
@@ -79,17 +83,26 @@
   (interactive)
   (compile compile-command))
 
-(defun zoo/haskell-add-keybindings ()
+(defun zoo/set-haskell-ghci-program
+  (if (zoo/is-cabal-dev-present?)
+      (if (and (executable-find "screen")
+               (executable-find "cabal-dev-ghci"))
+          (progn
+            (setq haskell-ghci-program-name "screen")
+            (setq haskell-ghci-program-args (list "-s" "cabal-dev-ghci")))
+        (progn
+          (setq haskell-ghci-program-name "cabal-dev")
+          (setq haskell-ghci-program-args (list "ghci"))))))
+
+(defun zoo/haskell-add-extra-keybindings ()
   (interactive)
-  (define-key haskell-mode-map
-    (kbd "<f4> b") 'zoo/haskell-compile) 
-  (define-key evil-normal-state-map
+  (evil-define-key 'normal haskell-mode-map
     (kbd ",b") 'zoo/haskell-compile))
 
 (defun zoo/haskell-mode-hook ()
   (interactive)
   (turn-on-haskell-doc-mode)
-  ;; (turn-on-haskell-simple-indent)
+  (turn-on-haskell-simple-indent)
   (zoo/haskell-set-compile-command)
   (zoo/flymake-haskell-load)
   (setq haskell-stylish-on-save t)
