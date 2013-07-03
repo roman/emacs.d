cb5aa2a8725efabce0c39f4a11dd8af76a71b0c1
Roman Gonzalez <roman@birdseye-sw.com>
2013-07-03 15:46:24 -0700

    Remove line-marks on whitespace-mode to avoid missbehavior of gnu screen

--- zoo/zoo-whitespace.el
+++ zoo/zoo-whitespace.el
@@ -1,29 +1,30 @@
-; whitespace always on
-(global-whitespace-mode 1)
-
-; the tab-width is 4 by default
+;; the tab-width is 4 by default
 (setq tab-width 4)
 
-; don't highlight lines with 8 spaces or more at the start
+;; don't highlight lines with 8 spaces or more at the start
 (setq-default indent-tabs-mode nil)
 
-; whitespace-mode should only show this properties
+;; whitespace-mode should only show this properties
 (setq whitespace-style
-      '(face tabs spaces trailing ;;lines-tail
-             space-before-tab
-             newline empty space-after-tab
-             space-mark tab-mark newline-mark))
+      '(face
+        tabs
+        spaces
+        trailing
+        newline
+        tab-mark
+        ;; newline-mark
+        empty
+        space-before-tab
+        space-after-tab))
 
-
-; special characters for newline and tabs
+;; special characters for newline and tabs
 (setq whitespace-display-mappings
-  '(
-     (space-mark ?\ [? ])
-     (newline-mark ?\n [?\u00AC ?\n])
-     (tab-mark ?\t [?\u25B8 ?\t])
-   ))
+      '(
+        (newline-mark 10 [194 172 10])
+        (tab-mark 9 [226 150 184 9])
+        ))
 
-; special colors for newline and tab character
+;; special colors for newline and tab character
 (custom-set-faces
  '(whitespace-space
    ((((class color) (background dark))
@@ -38,11 +39,15 @@
    ((((class color) (background dark))
      (:background "#111" :foreground "#111")))))
 
-; f4 should be whitespace-cleanup
+;; f4 should be whitespace-cleanup
 (global-set-key (kbd "<f4> SPC") 'whitespace-cleanup)
 (define-key evil-normal-state-map
   (kbd ", SPC") 'whitespace-cleanup)
 
 (add-hook 'after-save-hook 'whitespace-cleanup)
+
+;; whitespace always on
+;; fucks up screen
+(global-whitespace-mode 1)
 
 (provide 'zoo-whitespace)
